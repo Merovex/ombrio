a9b626f22c6a12fceb1b6b2985a6dde6cd0b2c00
Ben W <dausha@gmail.com>
2019-08-31 14:37:19 -0400

    feat(layout): Layout done for Editor with dark/light mode.

--- application/verkilo/app/components/Home.js
+++ application/verkilo/app/components/Home.js
@@ -1,19 +1,144 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
-import styles from './Home.css';
+// import { Link } from 'react-router-dom';
+// import routes from '../constants/routes';
+// import styles from './Home.scss';
 
-type Props = {};
-
-export default class Home extends Component<Props> {
-  props: Props;
-
+export default class Home extends Component {
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+      <div className="dark" data-tid="container">
+        <nav>
+          <div>
+            <ul>
+              <li>here</li>
+              <li>View / Group Mode</li>
+              <li>Compile</li>
+            </ul>
+          </div>
+          <div className="search">
+            Search: <input type='text' />
+          </div>
+        </nav>
+        <main>
+          <header>Page Title</header>
+          <aside className='binder'>
+            <h4>Binder</h4>
+              <ul>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <ul>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                </ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                  <li><input type="text" value="Page Title" /></li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                  <ul>
+                    <li><input type="text" value="Page Title" /></li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                  </ul>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                    <ul>
+                      <li>Page Title</li>
+                      <li>Page Title</li>
+                      <li>Page Title</li>
+                    </ul>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+                    <li>Page Title</li>
+              </ul>
+          </aside>
+          <article>
+            <textarea>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+            </textarea>
+          </article>
+          <aside className='metadata'>
+            <div>
+              <h4>Synopsis</h4>
+              <h5>Page Title</h5>
+              <textarea>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.
+              </textarea>
+            </div>
+            <div>
+              <h4>Metadata</h4>
+              <table>
+                <tr><th>Created:</th><td> April 1, 2019 at 8:24pm</td></tr>
+                <tr><th>Updated:</th><td> July 1, 2019 at 11:44am</td></tr>
+                <tr><th>Status:</th><td> First Draft</td></tr>
+              </table>
+            </div>
+            <div>
+              <h4>Keywords</h4>
+              <ul>
+                <li><span className='label'>Bophendze</span></li>
+                <li><span className='label'>Space station</span></li>
+                <li><input type="text" /></li>
+              </ul>
+            </div>
+            <div>
+              <h4>Notes</h4>
+              <textarea>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In massa tempor nec feugiat nisl pretium fusce id velit. Hac habitasse platea dictumst vestibulum rhoncus. Lacus vestibulum sed arcu non odio euismod lacinia at. Praesent tristique magna sit amet purus gravida quis blandit turpis. Egestas congue quisque egestas diam in arcu cursus euismod quis. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Nibh praesent tristique magna sit amet purus gravida quis. Amet cursus sit amet dictum sit amet justo. Et magnis dis parturient montes nascetur.</textarea>
+            </div>
+          </aside>
+        </main>
+        <footer>
+          <div>
+            Dark Mode
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round" />
+            </label>
+          </div>
+          <div className="wordcounts">
+            <span className="section">1,500</span> /
+            <span className="document"> 120,000</span> words
+          </div>
+          <div className="state">#First Draft</div>
+          <div className="mode">Focus</div>
+          <div className="zoom">Zoom</div>
+          <div className="sync">Sync</div>
+        </footer>
       </div>
     );
   }
