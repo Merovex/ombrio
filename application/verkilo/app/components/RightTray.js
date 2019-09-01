2ff564539b5c2092fc89be83b8b0890c8ed07324
Ben W <dausha@gmail.com>
2019-09-01 08:27:01 -0400

    feat(layout): Component breakdown, dark/light working.

---
+++ application/verkilo/app/components/RightTray.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+export default class RightTray extends Component {
+
+  render() {
+    return (
+      <aside className='binder'>
+        <h4>Binder</h4>
+          <ul>
+            <li>Page Title</li>
+            <li>Page Title</li>
+            <li>Page Title</li>
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+              <li>Page Title</li>
+              <li>Page Title</li>
+            </ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+            <li>Page Title</li>
+            <li>Page Title</li>
+            <li>Page Title</li>
+            <li>Page Title</li>
+            <li>Page Title</li>
+            <li>Page Title</li>
+              <li><input type="text" value="Page Title" /></li>
+              <li>Page Title</li>
+              <li>Page Title</li>
+              <ul>
+                <li><input type="text" value="Page Title" /></li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+              </ul>
+              <li>Page Title</li>
+              <li>Page Title</li>
+              <li>Page Title</li>
+              <li>Page Title</li>
+              <li>Page Title</li>
+              <li>Page Title</li>
+              <li>Page Title</li>
+              </ul>
+              <h4>Research</h4>
+              <ul>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <ul>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                  <li>Page Title</li>
+                </ul>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+                <li>Page Title</li>
+          </ul>
+      </aside>
+    );
+  }
+}
