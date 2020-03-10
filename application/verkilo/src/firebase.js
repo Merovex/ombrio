4d18543153b9815741e88aae744c2ad5b84f0117
Ben W <dausha@gmail.com>
2020-03-10 18:55:53 -0400

    configure Firebase.

--- application/verkilo/src/firebase.js
+++ application/verkilo/src/firebase.js
@@ -1,11 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseUrl: "",
-  projectId: "",
-  storageBucket: "",
-  messageSenderId: "",
-  ApiID: "",
+  apiKey: "AIzaSyCMReaN4IgyoBpfutv_51BBwS16OKaRvas",
+  authDomain: "verkilo-app.firebaseapp.com",
+  databaseURL: "https://verkilo-app.firebaseio.com",
+  projectId: "verkilo-app",
+  storageBucket: "verkilo-app.appspot.com",
+  messagingSenderId: "996700250031",
+  appId: "1:996700250031:web:7e7f0a488945423dfc195a",
+  measurementId: "G-621XSKKE77"
 })
 
 export {firebaseConfig as firebase};
